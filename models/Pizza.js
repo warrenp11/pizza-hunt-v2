3b8cd6fba6667bdbad9ba56dffecde460916b776
Peter <warrenp11@gmail.com>
2022-01-09 14:53:59 -0500

    install mongoose, import dependencies

---
+++ models/Pizza.js
@@ -0,0 +1,10 @@
+// Import the dependencies
+const { Schema, model } = require('mongoose');
+
+// Following data to be stored when users create a new pizza:
+// The name of the pizza
+// The name of the user that created the pizza
+// A timestamp of when the pizza was created
+// A timestamp of any updates to the pizza's data
+// The pizza's suggested size
+// The pizza's toppings
