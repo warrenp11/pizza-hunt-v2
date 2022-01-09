3773c8747616bb7c8ca09c1c929b27756c0a6460
Peter <warrenp11@gmail.com>
2022-01-09 15:05:02 -0500

    created pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,10 +1,27 @@
 // Import the dependencies
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-// Following data to be stored when users create a new pizza:
-// The name of the pizza
-// The name of the user that created the pizza
-// A timestamp of when the pizza was created
-// A timestamp of any updates to the pizza's data
-// The pizza's suggested size
-// The pizza's toppings
+// Create schema for Pizza
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
