bc261a987df47a5c90f0eed38e8013a1057d0f67
Peter <warrenp11@gmail.com>
2022-01-10 16:14:31 -0500

    update comment routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,9 +1,13 @@
-const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const router = require("express").Router();
+const {
+  addComment,
+  removeComment,
+} = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
+router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-
+router.route("/:pizzaId/:commentId").delete(removeComment);
 
 module.exports = router;
