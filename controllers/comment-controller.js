458e4c6adf1d0e728046a4fae3c0d6b07a38e843
Peter <warrenp11@gmail.com>
2022-01-10 10:51:07 -0500

    initial addComment logic

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,15 +1,19 @@
 const { Comment, Pizza } = require("../models");
 
 const commentController = {
-    // add comment to pizza
-    addComment() {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+    .then(({ _id }) => {
+      console.log(_id);
+      // Now that we've got an _id, we can use this to add the comment to a pizza. Do that by updating the addComment() method
+    });
+  },
 
-    },
 
-    // remove comment
-    removeComment() {
-
-    },
+  // remove comment
+  removeComment() {},
 };
 
 module.exports = commentController;
