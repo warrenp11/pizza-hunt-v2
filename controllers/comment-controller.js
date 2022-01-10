25483f37dd4a609f1a580828156fa87b5e914d57
Peter <warrenp11@gmail.com>
2022-01-10 10:53:00 -0500

    update addComment logic

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -5,10 +5,21 @@ const commentController = {
   addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
-    .then(({ _id }) => {
-      console.log(_id);
-      // Now that we've got an _id, we can use this to add the comment to a pizza. Do that by updating the addComment() method
-    });
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } }, // $push similar to js push which adds data to array
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
 
 
