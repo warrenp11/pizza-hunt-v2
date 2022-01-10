8e842d3690c54b692bd451eb3316d681beb42ae3
Peter <warrenp11@gmail.com>
2022-01-10 10:46:32 -0500

    add skeleton for comment controller

---
+++ controllers/comment-controller.js
@@ -0,0 +1,15 @@
+const { Comment, Pizza } = require("../models");
+
+const commentController = {
+    // add comment to pizza
+    addComment() {
+
+    },
+
+    // remove comment
+    removeComment() {
+
+    },
+};
+
+module.exports = commentController;
