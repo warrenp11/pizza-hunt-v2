e211c3dceb7152aab6298dabfeebdc21beb73df7
Peter <warrenp11@gmail.com>
2022-01-12 12:36:28 -0500

    update handlePizzaSubmit to make transaction if no connection

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -64,6 +64,8 @@ const handlePizzaSubmit = (event) => {
     })
     .catch((err) => {
       console.log(err);
+      // when user tries to submit new pizza without an internet connection, the data will be stored in the new_pizza object store in the browser's IndexedDB database called pizza_hunt
+      saveRecord(formData);
     });
 };
 
