4822c1c5342a69429fde3309b42f9d20ec308366
Peter <warrenp11@gmail.com>
2022-01-12 12:16:36 -0500

    add event listeners to the request object

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -10,3 +10,20 @@ request.onupgradeneeded = function (event) {
   // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_pizza", { autoIncrement: true });
 };
+
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    // uploadPizza();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
